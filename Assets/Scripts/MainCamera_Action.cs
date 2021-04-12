108ec404458a5deecff5aba2b863dd865ee2ff9b
ralskwo <ralskwo@naver.com>
2021-04-12 12:21:18 +0900

    mygames

---
+++ Assets/Scripts/MainCamera_Action.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera_Action : MonoBehaviour
+{
+    // 마우스 회전
+    /*public float turnSpeed = 4.0f; // 마우스 회전 속도    
+    private float xRotate = 0.0f; // 내부 사용할 X축 회전량은 별도 정의 ( 카메라 위 아래 방향 )
+    public float moveSpeed = 4.0f; // 이동 속도
+    public float jumpForce = 10.0f; // 점프하는 힘
+    private bool isGround = true; // 땅에 붙어있는가?
+    Rigidbody body; // Rigidbody를 가져올 변수
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        transform.rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        // 좌우로 움직인 마우스의 이동량 * 속도에 따라 카메라가 좌우로 회전할 양 계산
+        float yRotateSize = Input.GetAxis("Mouse X") * turnSpeed;
+        // 현재 y축 회전값에 더한 새로운 회전각도 계산
+        float yRotate = transform.eulerAngles.y + yRotateSize;
+
+        // 위아래로 움직인 마우스의 이동량 * 속도에 따라 카메라가 회전할 양 계산(하늘, 바닥을 바라보는 동작)
+        float xRotateSize = -Input.GetAxis("Mouse Y") * turnSpeed;
+        // 위아래 회전량을 더해주지만 -45도 ~ 80도로 제한 (-45:하늘방향, 80:바닥방향)
+        // Clamp 는 값의 범위를 제한하는 함수
+        xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);
+
+        // 카메라 회전량을 카메라에 반영(X, Y축만 회전)
+        transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+
+        //  키보드에 따른 이동량 측정
+        Vector3 move =
+            transform.forward * Input.GetAxis("Vertical") +
+            transform.right * Input.GetAxis("Horizontal");
+
+        // 이동량을 좌표에 반영
+        transform.position += move * moveSpeed * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGround)
+        {
+            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            isGround = false;
+        }
+    }
+
+    // 충돌 함수
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }*/
+
+    // 고정 따라가기
+    //public GameObject Player;
+    public GameObject Target;               // 카메라가 따라다닐 타겟
+
+    public float offsetX = 0.0f;            // 카메라의 x좌표
+    public float offsetY = 10.0f;           // 카메라의 y좌표
+    public float offsetZ = -10.0f;          // 카메라의 z좌표
+
+    public float CameraSpeed = 10.0f;       // 카메라의 속도
+    Vector3 TargetPos;                      // 타겟의 위치
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        // 타겟의 x, y, z 좌표에 카메라의 좌표를 더하여 카메라의 위치를 결정
+        TargetPos = new Vector3(
+            Target.transform.position.x + offsetX,
+            Target.transform.position.y + offsetY,
+            Target.transform.position.z + offsetZ
+            );
+
+        // 카메라의 움직임을 부드럽게 하는 함수(Lerp)
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * CameraSpeed);
+    }
+
+
+    // 회전 따라가기
+    /*public Transform target;
+    public float distance = 10.0f;
+    public float height = 5.0f;
+    private float rotationDamping = 3.0f;
+    private float heightDamping = 2.0f;
+
+    void LateUpdate()
+    {
+        if (!target)
+
+            return;
+
+        var wantedRotationAngle = target.eulerAngles.y;
+        var wantedHeight = target.position.y + height;
+
+        var currentRotationAngle = transform.eulerAngles.y;
+        var currentHeight = transform.position.y;
+
+
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        transform.position = target.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        transform.LookAt(target);
+    }*/
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 10.0f;
+    public float rotateSpeed = 10.0f;
+    public float jumpForce = 10.0f;
+
+    private float h, v;
+
+    private bool isGround = true;
+
+    Rigidbody body;
+
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+
+        if (!(h == 0 && v == 0))
+        {
+            transform.position += dir.normalized * Speed * Time.deltaTime;
+            //transform.Translate(dir * Speed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotateSpeed);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGround)
+        {
+            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            isGround = false;
+        }
+    }
+
+    // 충돌 함수
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+}
