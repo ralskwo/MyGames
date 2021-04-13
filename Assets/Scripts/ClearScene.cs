c0286c5949c2a7daaeefbe5227e3490ac3dea69b
ralskwo <ralskwo@naver.com>
2021-04-13 22:15:08 +0900

    done

--- Assets/Scripts/ClearScene.cs
+++ Assets/Scripts/ClearScene.cs
@@ -10,7 +10,7 @@ public class ClearScene : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-        Application.Quit() // 어플리케이션 종료
+        Application.Quit(); // 어플리케이션 종료
 #endif
     }
 
